5c22195c0254575127442dbfe92c3c459225eeeb
GabrielGarciaToriz <gabrielgtoriz@hotmail.com>
2026-05-22 12:35:06 -0600

    Correccion de entidades

--- src/main/java/com/digis/GGarciaBanco/entity/RetiroDetalle.java
+++ src/main/java/com/digis/GGarciaBanco/entity/RetiroDetalle.java
@@ -1,5 +1,25 @@
 package com.digis.GGarciaBanco.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RetiroDetalle {
-    
+
+    private Integer idRetiro;
+    private Retiro retiro;
+
+    private Integer idDenominacion;
+    private Denominacion deominacion;
+
+    private Integer cantidad;
+    private Long subtotalCentavos;
+
 }
